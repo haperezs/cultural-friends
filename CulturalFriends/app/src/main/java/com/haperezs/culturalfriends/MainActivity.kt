1d4e8eff20ce0f26658ca0bb9550aac18bb3d7e5
Hugo P <haperezs@hotmail.com>
2025-04-11 18:12:47 -0600

    Update theme

--- CulturalFriends/app/src/main/java/com/haperezs/culturalfriends/MainActivity.kt
+++ CulturalFriends/app/src/main/java/com/haperezs/culturalfriends/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ChatBubbleOutline
@@ -12,12 +12,12 @@ import androidx.compose.material.icons.outlined.Map
 import androidx.compose.material.icons.outlined.Translate
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -37,8 +37,10 @@ class BottomNavigationItem(
 )
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             CulturalFriendsTheme {
                 val items = listOf(
@@ -63,43 +65,46 @@ class MainActivity : ComponentActivity() {
                 )
                 val navController = rememberNavController()
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(
-                        bottomBar = {
-                            NavigationBar {
-                                items.forEachIndexed { index, item ->
-                                    NavigationBarItem(
-                                        selected = false,
-                                        onClick = {
-                                            Log.d(javaClass.simpleName,"Navigate to ${item.title}")
-                                            navController.navigate(item.route)
-                                        },
-                                        label = {
-                                            Text(text = item.title)
-                                        },
-                                        alwaysShowLabel = true,
-                                        icon = {
-                                            BadgedBox(
-                                                badge = {
-                                                    if(item.badge) {
-                                                        Badge()
-                                                    }
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(text = "Topbar")
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    selected = false,
+                                    onClick = {
+                                        Log.d(javaClass.simpleName,"Navigate to ${item.title}")
+                                        navController.navigate(item.route)
+                                    },
+                                    label = {
+                                        Text(text = item.title)
+                                    },
+                                    alwaysShowLabel = true,
+                                    icon = {
+                                        BadgedBox(
+                                            badge = {
+                                                if(item.badge) {
+                                                    Badge()
                                                 }
-                                            ) {
-                                                Icon(
-                                                    imageVector = item.icon,
-                                                    contentDescription = item.title
-                                                )
                                             }
+                                        ) {
+                                            Icon(
+                                                imageVector = item.icon,
+                                                contentDescription = item.title
+                                            )
                                         }
-                                    )
-                                }
+                                    }
+                                )
                             }
                         }
-                    ) { innerPadding ->
+                    },
+                    content = { innerPadding ->
                         NavHost(
                             navController = navController,
                             startDestination  = Screen.FinderScreen.route,
@@ -110,7 +115,7 @@ class MainActivity : ComponentActivity() {
                             composable(Screen.TranslateScreen.route) { TranslateScreen(navController) }
                         }
                     }
-                }
+                )
             }
         }
     }
